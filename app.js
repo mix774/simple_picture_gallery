eb63448e6bdb71468931c3ba5637df3d8428162a
mix774 <nikitami2607@gmail.com>
2022-06-06 15:37:59 +0300

    Функционал реализован, как плагин. Добавлены комментарии.

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
-const slides = document.querySelectorAll('.slide')
+function slidesPlugin(activeSlide = 0) {
+	const slides = document.querySelectorAll('.slide') //получение массива элементов с классом slide
 
-for (const slide of slides) {
-	slide.addEventListener('click', () => {
-		clearActiveClasses()
-		slide.classList.add('active')
-	})
+	slides[activeSlide].classList.add('active')
+
+	//итерация по массиву с полученными элементами, по щелчку мыши вызов функции, 
+	//которая удаляет стили с текущего элемента, и добавляет стили на тот элемент, по которому кликнули
+	for (const slide of slides) {
+		slide.addEventListener('click', () => {
+			clearActiveClasses()
+			slide.classList.add('active')
+		})
+	}
+
+
+	//функция, которая итерируется по массиву с элементами и удаляет стиль для выделения картинки
+	function clearActiveClasses() {
+		slides.forEach((slide) => {
+			slide.classList.remove('active')
+		})
+	}
 }
 
-function clearActiveClasses() {
-	slides.forEach((slide) => {
-		slide.classList.remove('active')
-	})
-}
+slidesPlugin()
